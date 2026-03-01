43fa5c7698c09424c622bc9982539c31809bd5c9
Maestro8701 <149194877+Maestro8701@users.noreply.github.com>
2026-03-01 23:49:20 +0700

    Add LinkedList class with serialization and deserialization

---
+++ CurrentTestWork/list.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <cstdint>
+#include <string>
+#include <vector>
+#include <memory>
+
+class LinkedList {
+public:
+    LinkedList() = default;
+
+    ~LinkedList() = default;
+
+    void Build(const std::vector<std::pair<std::string, int32_t>>& entries);
+
+    LinkedList(const LinkedList&) = delete;
+
+    LinkedList& operator=(const LinkedList&) = delete;
+
+    bool Serialize(const std::string& filename) const;
+
+    bool Deserialize(const std::string& filename);
+
+    bool Compare(const std::vector<std::pair<std::string, int32_t>>& original) const;
+
+    void DumpText(const std::string& filename) const;
+
+private:
+    struct ListNode {
+        ListNode* prev = nullptr;
+        ListNode* next = nullptr;
+        ListNode* rand = nullptr;
+        std::string data;
+    };
+
+    std::vector< std::unique_ptr<ListNode>> nodes_;
+
+};
+
